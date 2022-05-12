f78a691e659a42c38b449ece33a76b42b4033c30
Lertos <deetasticgaming@gmail.com>
2022-05-12 17:58:54 -0300

    Adding enemy stat values

--- scripts/global_data/enemies.gd
+++ scripts/global_data/enemies.gd
@@ -1,46 +1,210 @@
 extends Node
 
+"""
+'stats': {
+				'atk_stab': 0,
+				'atk_slash': 0,
+				'atk_crush': 0,
+				'def_stab': 0,
+				'def_slash': 0,
+				'def_crush': 0,
+				'health': 0,
+				'atk_speed': 0,
+				'dmg_reduc': 0,
+				'min_hit': 0,
+				'max_hit': 0,
+				'abilities': {
+					0: {
+						'name': 'Fire Breath',
+						'description': 'Each round take 2% of your maximum HP as damage',
+						'chance': '60'
+					}
+				}
+			},
+"""
+
+
 var enemies = {
 	'valley': {
 		0: {
 			'id': 'valley_chicken',
 			'name': 'Farm Chicken',
 			'level': 1,
+			'stats': {
+				'atk_stab': 1,
+				'atk_slash': 2,
+				'atk_crush': 3,
+				'def_stab': 4,
+				'def_slash': 5,
+				'def_crush': 6,
+				'health': 100,
+				'atk_speed': 1.2,
+				'dmg_reduc': 4.5,
+				'min_hit': 23,
+				'max_hit': 87,
+				'abilities': {
+					0: {
+						'name': 'Fire Breath',
+						'description': 'Take 15% additional damage from the monster hit',
+						'chance': '60'
+					},
+					1: {
+						'name': 'Rooster Bark',
+						'description': 'You lose your turn',
+						'chance': '20'
+					},
+				}
+			},
+			'drops': {
+				#TODO
+			}
 		},
 		1: {
 			'id': 'valley_rabbit',
 			'name': 'Wild Rabbit',
 			'level': 3,
+			'stats': {
+				'atk_stab': 1,
+				'atk_slash': 2,
+				'atk_crush': 3,
+				'def_stab': 4,
+				'def_slash': 5,
+				'def_crush': 6,
+				'health': 100,
+				'atk_speed': 1.2,
+				'dmg_reduc': 4.5,
+				'min_hit': 23,
+				'max_hit': 87,
+				'abilities': {
+
+				}
+			},
 		},
 		2: {
 			'id': 'valley_crab',
 			'name': 'Elusive Crab',
 			'level': 5,
+			'stats': {
+				'atk_stab': 1,
+				'atk_slash': 2,
+				'atk_crush': 3,
+				'def_stab': 4,
+				'def_slash': 5,
+				'def_crush': 6,
+				'health': 100,
+				'atk_speed': 1.2,
+				'dmg_reduc': 4.5,
+				'min_hit': 23,
+				'max_hit': 87,
+				'abilities': {
+
+				}
+			},
 		},
 		3: {
 			'id': 'valley_goat',
 			'name': 'Noisy Goat',
 			'level': 7,
+			'stats': {
+				'atk_stab': 1,
+				'atk_slash': 2,
+				'atk_crush': 3,
+				'def_stab': 4,
+				'def_slash': 5,
+				'def_crush': 6,
+				'health': 100,
+				'atk_speed': 1.2,
+				'dmg_reduc': 4.5,
+				'min_hit': 23,
+				'max_hit': 87,
+				'abilities': {
+
+				}
+			},
 		},
 		4: {
 			'id': 'valley_elemental',
 			'name': 'Earth Elemental',
 			'level': 9,
+			'stats': {
+				'atk_stab': 1,
+				'atk_slash': 2,
+				'atk_crush': 3,
+				'def_stab': 4,
+				'def_slash': 5,
+				'def_crush': 6,
+				'health': 100,
+				'atk_speed': 1.2,
+				'dmg_reduc': 4.5,
+				'min_hit': 23,
+				'max_hit': 87,
+				'abilities': {
+
+				}
+			},
 		},
 		5: {
 			'id': 'valley_cat',
 			'name': 'Rabid Cat',
 			'level': 11,
+			'stats': {
+				'atk_stab': 1,
+				'atk_slash': 2,
+				'atk_crush': 3,
+				'def_stab': 4,
+				'def_slash': 5,
+				'def_crush': 6,
+				'health': 100,
+				'atk_speed': 1.2,
+				'dmg_reduc': 4.5,
+				'min_hit': 23,
+				'max_hit': 87,
+				'abilities': {
+
+				}
+			},
 		},
 		6: {
 			'id': 'valley_slime',
 			'name': 'Earthy Slime',
 			'level': 13,
+			'stats': {
+				'atk_stab': 1,
+				'atk_slash': 2,
+				'atk_crush': 3,
+				'def_stab': 4,
+				'def_slash': 5,
+				'def_crush': 6,
+				'health': 100,
+				'atk_speed': 1.2,
+				'dmg_reduc': 4.5,
+				'min_hit': 23,
+				'max_hit': 87,
+				'abilities': {
+
+				}
+			},
 		},
 		7: {
 			'id': 'valley_snake',
 			'name': 'Giant Python',
 			'level': 15,
+			'stats': {
+				'atk_stab': 1,
+				'atk_slash': 2,
+				'atk_crush': 3,
+				'def_stab': 4,
+				'def_slash': 5,
+				'def_crush': 6,
+				'health': 100,
+				'atk_speed': 1.2,
+				'dmg_reduc': 4.5,
+				'min_hit': 23,
+				'max_hit': 87,
+				'abilities': {
+					
+				}
+			},
 		},
 	}
 }
--- scripts/popup_monster_info.gd
+++ scripts/popup_monster_info.gd
@@ -3,15 +3,52 @@ extends Node
 
 func _ready():
 	var monster = self.get_meta('monster')
-	var parent_node = get_node('vbox/header')
 	
-	parent_node.get_node('vbox/name').text = monster['name']
-	parent_node.get_node('vbox/hbox/level').text = str(monster['level'])
+	#Add Header Info
+	var header = get_node('vbox/header')
+	header.get_node('vbox/name').text = monster['name']
+	header.get_node('vbox/hbox/level').text = str(monster['level'])
 	
+	#Add Stat Info
+	var stats = get_node('vbox/stats')
+	
+	var left_panel = stats.get_node('left_panel/values')
+	
+	left_panel.get_node('atk_stab').text = str(monster['stats']['atk_stab'])
+	left_panel.get_node('atk_slash').text = str(monster['stats']['atk_slash'])
+	left_panel.get_node('atk_crush').text = str(monster['stats']['atk_crush'])
+	
+	left_panel.get_node('health').text = str(monster['stats']['health'])
+	left_panel.get_node('atk_speed').text = str(monster['stats']['atk_speed']) + 's'
+	left_panel.get_node('dmg_reduc').text = str(monster['stats']['dmg_reduc']) + '%'
+	
+	var right_panel = stats.get_node('right_panel/values')
+	
+	right_panel.get_node('def_stab').text = str(monster['stats']['def_stab'])
+	right_panel.get_node('def_slash').text = str(monster['stats']['def_slash'])
+	right_panel.get_node('def_crush').text = str(monster['stats']['def_crush'])
+	
+	right_panel.get_node('min_hit').text = str(monster['stats']['min_hit'])
+	right_panel.get_node('max_hit').text = str(monster['stats']['max_hit'])
+	
+	#Abilities
+	var abilities = get_node('vbox/bottom_panel/abilities/abilities')
+	
+	for ability in monster['stats']['abilities']:
+		var current_ability = monster['stats']['abilities'][ability]
+
+		abilities.bbcode_text += '([color=yellow]' + str(current_ability['chance']) + '%[/color]) '
+		abilities.bbcode_text += '[color=red]' + str(current_ability['name']) + '[/color] '
+		abilities.bbcode_text += str(current_ability['description'])
+		
+		abilities.bbcode_text += '\n\n'
+	
+	#Add Texture
+	var monster_image = get_node('vbox/header/monster')
 	var animated_tex = AnimatedTexture.new()
 	animated_tex.frames = 2
 	animated_tex.fps = 2
 	animated_tex.set_frame_texture(0, load('res://assets/monsters/' + monster['id'] + ' (1).png'))
 	animated_tex.set_frame_texture(1, load('res://assets/monsters/' + monster['id'] + ' (2).png'))
 	
-	parent_node.get_node('monster').texture = animated_tex
+	monster_image.texture = animated_tex
