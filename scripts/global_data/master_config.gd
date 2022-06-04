837bbe1a780f05385d8ebd6a8ac02fc33fc3b76d
Lertos <deetasticgaming@gmail.com>
2022-06-04 11:44:13 -0300

    Adding master config

---
+++ scripts/global_data/master_config.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var config = {
+	'rarities': {
+		'common': {
+			'order': 0,
+		},
+		'uncommon': {
+			'order': 1,
+		},
+		'rare': {
+			'order': 2,
+		},
+		'legendary': {
+			'order': 3,
+		},
+		'mythic': {
+			'order': 4,
+		},
+	}
+}
