1ea9f4a4fb1dbd6548865dc5a894a6237861732d
Lertos <deetasticgaming@gmail.com>
2022-06-18 14:43:38 -0300

    Making the message popup look better

--- scripts/popups/message_popup.gd
+++ scripts/popups/message_popup.gd
@@ -4,6 +4,12 @@ const SIDE_MARGIN_WIDTH = 32
 
 func _ready():
 	self.rect_min_size.x = get_viewport().size.x - SIDE_MARGIN_WIDTH
+	
+	#Expand and fill the button
+	self.get_ok().set_h_size_flags(3)
+	
+	#Center the text
+	self.get_child(1).align = HALIGN_CENTER
 
 
 func set_text(text):
