d36e1e02ba1b681c3fc2039e92d23d5b7e668a67
Lertos <deetasticgaming@gmail.com>
2022-07-09 21:52:09 -0300

    Adding base functionality for talent section

--- scripts/screens/screen_heroes.gd
+++ scripts/screens/screen_heroes.gd
@@ -3,6 +3,7 @@ extends MarginContainer
 var locked_texture = load('res://assets/icons/icon12.png')
 
 var hero_timers = {}
+var spent_points = {}
 
 
 func _ready():
@@ -19,9 +20,13 @@ func load_screen():
 
 func select_hero(index: int):
 	if index in Global_Player.player['heroes']:
-		print('exists')
+		load_hero_sections(index, Global_Player.player['heroes'][index])
 	else:
 		open_create_hero_popup(index)
+
+
+func load_hero_sections(index: int, hero_info):
+	load_hero_talents_section(index, hero_info)
 
 
 func open_create_hero_popup(index: int):
@@ -52,6 +57,9 @@ func load_heroes():
 			else:
 				hero_record.get_node('img').icon = locked_texture
 				hero_record.get_node('img').disabled = true
+				
+	if len(hero_info) > 0:
+		load_hero_sections(0, hero_info[0])
 
 
 func load_hero_info(hero_record, hero_info, index):
@@ -93,3 +101,76 @@ func update_hero_timers():
 		else:
 			hero_timers[hero]['time_left'] = new_time
 			path.get_node(str(hero) + '/status').text = Helper.get_time_from_stamp(new_time)
+			
+			
+func load_hero_talents_section(index, hero_info):
+	#TODO: Load available talent points based on hero key for available talents
+	var amounts_node = get_node('parent_vbox/margin/talents/vbox/hbox/amounts')
+	var buttons_node = get_node('parent_vbox/margin/talents/vbox/hbox/buttons')
+	var descriptions_node = get_node('parent_vbox/margin/talents/vbox/hbox/descriptions')
+	
+	var descriptions = MasterConfig.config['talent_bonuses']
+	
+	undo_spent_points()
+	
+	for talent in hero_info['talent_bonuses']:
+		var current_bonus = hero_info['talent_bonuses'][talent]
+		var description = descriptions[talent]
+		
+		var new_amount = amounts_node.get_node('default').duplicate()
+		var new_button = buttons_node.get_node('default').duplicate()
+		var new_description = descriptions_node.get_node('default').duplicate()
+		
+		amounts_node.add_child(new_amount)
+		buttons_node.add_child(new_button)
+		descriptions_node.add_child(new_description)
+		
+		new_amount.visible = true
+		new_button.visible = true
+		new_description.visible = true
+		
+		if talent.substr(0,1) == 'n':
+			new_amount.text = str(current_bonus)
+		else:
+			new_amount.text = str(current_bonus) + '%'
+		
+		new_amount.name = talent
+		new_button.connect('pressed', self, 'increase_talent', [int(index), talent])
+		new_description.text = description
+
+
+func increase_talent(index: int, talent):
+	var available_points = get_node('parent_vbox/margin/talents/vbox/vbox/talent_points/amount')
+	var amounts_node = get_node('parent_vbox/margin/talents/vbox/hbox/amounts')
+	
+	if !available_points.text.is_valid_integer():
+		get_node('/root/root').show_message_popup('The amount for the available points is not an integer')
+		return
+	if int(available_points.text) <= 0:
+		get_node('/root/root').show_message_popup('You do not have enough available points')
+		return
+		
+	available_points.text = str(int(available_points.text) - 1)
+	
+	for child in amounts_node.get_children():
+		if child.name == talent:
+			#TODO: Get this value from the talent master config "increase per level"
+			child.text = str(int(child.text) + 1)
+			
+			if !(talent in spent_points):
+				spent_points[talent] = 0
+				
+			spent_points[talent] = spent_points[talent] + 1
+
+
+func confirm_spent_points():
+	pass
+
+
+func undo_spent_points():
+	#TODO: Loop through each dict key and subtract those points from the amounts labels, then add points back
+	spent_points.clear()
+	
+	
+func reset_total_points():
+	pass
