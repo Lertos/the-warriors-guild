31a13ed66919c9a44f5ba19c64792fc0ed7feb50
Lertos <deetasticgaming@gmail.com>
2022-08-07 12:32:29 -0300

    Vendor icons were too small; not consistent

--- scripts/record_templates/vendor_record.gd
+++ scripts/record_templates/vendor_record.gd
@@ -4,6 +4,6 @@ func update_record():
 	var parent = get_node('margin/hbox')
 	var vendor = self.get_meta('vendor')
 	
-	parent.get_node('monster').icon = load('res://assets/vendors/' + vendor['img'] + '.png')
+	parent.get_node('vendor').icon = load('res://assets/vendors/' + vendor['img'] + '.png')
 	
 	parent.get_node('info/name').text = vendor['name']
