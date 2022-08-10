e791ddcf4801db61d78883ca8bb26f17db2b3c57
Lertos <deetasticgaming@gmail.com>
2022-08-09 22:05:58 -0300

    Adding stats module to hero stats section

--- scripts/screens/heroes/section_stats.gd
+++ scripts/screens/heroes/section_stats.gd
@@ -7,7 +7,8 @@ func load_hero_stats_section(index: int, hero_info: Dictionary):
 	var total_stats = Helper.get_hero_total_stats(hero_info)
 	
 	fill_hero_base_info(total_stats, hero_info)
-	fill_stats_panel(total_stats)
+	
+	parent_node.get_node('stat_module').build(total_stats)
 
 
 func fill_hero_base_info(total_stats, hero_info):
@@ -25,20 +26,3 @@ func fill_hero_base_info(total_stats, hero_info):
 	
 	base_info_node.get_node('info2/health/current_health').text = str(hero_info['current_health'])
 	base_info_node.get_node('info2/health/max_health').text = str(total_stats['health'])
-
-
-func fill_stats_panel(total_stats):
-	var left_panel = parent_node.get_node('hbox2/left_panel/values')
-	var right_panel = parent_node.get_node('hbox2/right_panel/values')
-	
-	fill_stat_nodes(total_stats, left_panel)
-	fill_stat_nodes(total_stats, right_panel)
-
-
-func fill_stat_nodes(total_stats, list_node):
-	for child in list_node.get_children():
-		var stat_key = child.name
-		
-		if !('spacer' in stat_key):
-			if stat_key in total_stats:
-				child.text = str(total_stats[stat_key])
