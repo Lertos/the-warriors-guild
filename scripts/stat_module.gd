54031fe11edcc5b20b4920780426f75046d9ca59
Lertos <deetasticgaming@gmail.com>
2022-08-11 22:16:43 -0300

    If a stat is zero, don't show it either

--- scripts/stat_module.gd
+++ scripts/stat_module.gd
@@ -16,15 +16,18 @@ func update_values(node: Node, dict: Dictionary, main_stat: String):
 		var stat_key = child.name
 		
 		if stat_key in dict:
-			child.get_node(stat_key).text = str(dict[stat_key])
-			
-			if stat_key == 'dmg_reduc':
-				child.get_node(stat_key).text += '%'
-			elif stat_key == 'atk_speed':
-				child.get_node(stat_key).text += 's'
+			if str(dict[stat_key]) == '0':
+				child.visible = false
+			else:
+				child.get_node(stat_key).text = str(dict[stat_key])
 				
-			if main_stat != '':
-				if main_stat == stat_key:
-					Helper.change_label_font_color(child.get_node(stat_key), 'stat_highlight')
+				if stat_key == 'dmg_reduc':
+					child.get_node(stat_key).text += '%'
+				elif stat_key == 'atk_speed':
+					child.get_node(stat_key).text += 's'
+					
+				if main_stat != '':
+					if main_stat == stat_key:
+						Helper.change_label_font_color(child.get_node(stat_key), 'stat_highlight')
 		else:
 			child.visible = false
