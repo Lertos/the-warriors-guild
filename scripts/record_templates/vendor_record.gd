cf91a22145012b40127cea7fc65f852d2dd1c6ec
Lertos <deetasticgaming@gmail.com>
2022-08-20 22:58:58 -0300

    Adding signal to open single vendor screen

--- scripts/record_templates/vendor_record.gd
+++ scripts/record_templates/vendor_record.gd
@@ -1,10 +1,16 @@
 extends PanelContainer
 
-func update_record():
-	var parent = get_node('margin/hbox')
-	var vendor = self.get_meta('vendor')
+onready var parent = get_node('margin/hbox')
+
+var vendor
+
+
+func setup_record(parent_script_node):
+	vendor = self.get_meta('vendor')
 	
 	parent.get_node('vendor_icon').icon = load('res://assets/vendors/icons/' + vendor['icon_img'] + '.png')
 	parent.get_node('vendor_avatar').icon = load('res://assets/vendors/avatars/' + vendor['avatar_img'] + '.png')
 	
 	parent.get_node('info/name').text = vendor['name']
+	
+	parent.get_node('vendor_icon').connect('pressed', parent_script_node, 'load_vendor_screen', [vendor['id']])
