a4a3829a8057bb7e3973485bf80bf6cd6efd0e79
Lertos <deetasticgaming@gmail.com>
2022-08-21 21:45:12 -0300

    Adding vendor screens and changing vendor buttons

--- scripts/screens/vendors/screen_vendors.gd
+++ scripts/screens/vendors/screen_vendors.gd
@@ -13,8 +13,28 @@ func load_vendors():
 		var vendor = Global_Vendors.vendors[index]
 		
 		var inst_vendor_record = scene_vendor_record.instance()
+		var single_vendor_screen = get_vendor_node(vendor['id'])
+		
+		get_node('parent_vbox/vendors/vbox').add_child(inst_vendor_record)
 		
 		inst_vendor_record.set_meta('vendor', vendor)
-		inst_vendor_record.update_record()
+		inst_vendor_record.setup_record(self)
+		
+		single_vendor_screen.set_meta('vendor', vendor)
+		single_vendor_screen.setup_screen(self)
 
-		get_node('parent_vbox/vendors/vbox').add_child(inst_vendor_record)
+
+func load_vendor_screen(vendor_id: String):
+	get_vendor_node(vendor_id).setup_screen(self)
+	
+	get_node('parent_vbox/vendors').visible = false
+	get_vendor_node(vendor_id).visible = true
+
+
+func show_all_vendors(vendor_id: String):
+	get_vendor_node(vendor_id).visible = false
+	get_node('parent_vbox/vendors').visible = true
+	
+
+func get_vendor_node(vendor_id: String):
+	return get_node('parent_vbox/' + vendor_id)
